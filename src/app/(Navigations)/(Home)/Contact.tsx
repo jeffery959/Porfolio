a9bc509a2b16ab8b8455c3b65669e6ab56575840
jeffery959 <aaronjeffery529@gmail.com>
2024-06-28 16:19:51 -0400

    fixed navbar

--- src/app/(Navigations)/(Home)/Contact.tsx
+++ src/app/(Navigations)/(Home)/Contact.tsx
@@ -12,7 +12,7 @@ const Contact = () => {
 
   const Submit=()=>{
     if(user.Name===''||user.Email===''||user.Message===''){
-      toast.error('Please fill out first three forms')
+      toast.error('Please fill out first three fields')
       return
     }
 
@@ -35,11 +35,11 @@ const Contact = () => {
     <div className="bg-secondary w-full md:mt-6 mt-8 flex justify-center items-center">
      <div className="w-full max-w-448 flex  items-center md:items-start  gap-3  py-20 flex-wrap  Divpadding flex-col">
       <p className="text-white font-semibold text-3xl text-center md:text-start mb-6">Get In Touch</p>
-      <input type="text" name='Name' value={user.Name} onChange={(e)=>handleUserForm(e)} className=" py-4 px-4 md:w-1/2 w-full md:max-w-128" placeholder="Name" />
-      <input type="text" name='Email' value={user.Email} onChange={(e)=>handleUserForm(e)} className=" py-4 px-4 md:w-1/2 w-full md:max-w-128" placeholder="Email Address" />
-     <textarea name="Message" onChange={(e)=>handleUserForm(e)} value={user.Message} id="" cols={30} rows={0} className=" py-4 px-4 w-full md:w-9/12 max-w-384 h-96" placeholder="Message"></textarea>
-      <input type="text" name='Budget' value={user.Budget} onChange={(e)=>handleUserForm(e)} className=" py-4 px-4 md:w-1/2 w-full md:max-w-128" placeholder="Budget" />
-      <button className="p-4 md:w-auto w-full px-8 bg-blue-600 hover:bg-blue-500 text-white" onClick={()=>Submit()}>Submit</button>
+      <input type="text" name='Name' value={user.Name} onChange={(e)=>handleUserForm(e)} className=" rounded-md py-4 px-4 md:w-1/2 w-full md:max-w-128" placeholder="Name" />
+      <input type="text" name='Email' value={user.Email} onChange={(e)=>handleUserForm(e)} className="rounded-md py-4 px-4 md:w-1/2 w-full md:max-w-128" placeholder="Email Address" />
+     <textarea name="Message" onChange={(e)=>handleUserForm(e)} value={user.Message} id="" cols={30} rows={0} className="rounded-md py-4 px-4 w-full md:w-9/12 max-w-384 h-96" placeholder="Message"></textarea>
+      <input type="text" name='Budget' value={user.Budget} onChange={(e)=>handleUserForm(e)} className="rounded-md py-4 px-4 md:w-1/2 w-full md:max-w-128" placeholder="Budget" />
+      <button className="rounded-md p-4 md:w-auto w-full px-8 bg-blue-600 hover:bg-blue-500 text-white" onClick={()=>Submit()}>Submit</button>
       <div className='flex flex-wrap gap-4 text-white'>
         <p>Email: aaronjeffery529@gmail.com</p>
         <p>Phone: +1(646)-627-0936</p>
--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useEffect,useState,useRef } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import { RouteUrl } from '../Utils/data'
 const routes = ['Home','About','Skills','Portfolio','Education','Contact']
 const Navbar = () => {
@@ -8,7 +9,7 @@ const Navbar = () => {
     const [navPading, setnavPading] = useState<null|boolean>(null);
     const [navName,setNavName] = useState('Home')
     const [activeSection, setActiveSection] = useState(null);
-
+    const [toggleNav,setToggleNav]=useState<boolean>(false)
     
     const changeNavColor=(txt:string)=>{
       setNavName(txt)
@@ -45,11 +46,11 @@ const Navbar = () => {
       };
     }, [navPading]);
     return (
-      <div className={`flex  ${navPading&&'md:pt-4'} duration-500 z-50 justify-between md:px-12 2xl:px-36 xl:px-24 md:py-12 py-12 px-2 sm:px-6 border-b fixed w-full bg-white top-0`}>
+      <div className={`flex  ${navPading&&'md:pt-16'} duration-500 z-50 justify-between md:px-12 2xl:px-36 xl:px-24 md:py-12 py-12 px-4 sm:px-6 border-b fixed w-full bg-white top-0`}>
         <h6 className='font-bold text-xl '>Aaron</h6>
-        <ul className='flex w-9/12 justify-between max-w-128'>
+        <ul className={`flex flex-col md:flex-row absolute md:relative md:right-auto right-6 top-20 md:top-auto md:shadow-none  bg-white shadow-lg md:p-0 py-4 px-4 rounded-lg md:w-9/12 justify-between max-w-128  ${!toggleNav&&'hidden md:flex'} `}>
             {routes.map((itm,id)=>{
-          const navCss=`${itm===navName?'text-secondary':' '} font-medium `
+          const navCss=`${itm===navName?'text-secondary':' '} font-medium hover:bg-gray-100 p-2 rounded-lg`
              
             
                return <Link key={id} href={`#${itm}`} onClick={()=>changeNavColor(itm)} className={navCss}>{itm}</Link>
@@ -59,6 +60,7 @@ const Navbar = () => {
             }
            
         </ul>
+        <Image src={'/Icons/Menu.svg'} width={20} height={20 } alt='' className='md:hidden' onClick={()=>setToggleNav(!toggleNav)}/>
         
     </div>
   )
